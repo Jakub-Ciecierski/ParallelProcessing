ef8220a1650de893450674222df8c79a2d5f8e99
j.ciecierski <jakub.ciecier@gmail.com>
2014-10-19 22:24:44 +0200

    Final version

--- project1/src/Student.java
+++ project1/src/Student.java
@@ -21,7 +21,7 @@ public class Student extends Thread{
 				// announces that he is interested in taking products
 				Main.s_table.acquire();
 				Main.student_queue_counter++;
-				releaseParticipants();
+				Main.releaseParticipants();
 				System.out.println("***** Student: " + id + " announced interest in products *****");
 				Main.s_table.release();
 				
@@ -81,81 +81,5 @@ public class Student extends Thread{
 		Main.virtual_sugar_consumption--;
 		Main.milk--;
 		Main.sugar--;
-	}
-	
-	/**
-	 * Checks if awaiting participants are allowed
-	 * to be released to take their products
-	 */
-	private void releaseParticipants() {
-		/*
-		 * Whenever a participant is waken up by a waiter he has to
-		 * wait for the waiter to finish checking every
-		 * set of products availability to release the main
-		 * s_table semaphore.
-		 * 
-		 * Thus if a waiter woke somebody up, further checking of products
-		 * has to include the fact that some will be missing.
-		 * 
-		 * (Main.coffee - Main.virtual_coffee_consumption) shows the true count
-		 * of coffee in the system. 
-		 * 
-		 */
-		for (int i = 0; i < Main.PROFESSOR_COUNT; i++) {
-			if (Main.professor_queue_counter > 0
-					&& (Main.coffee - Main.virtual_coffee_consumption) > 0
-					&& (Main.milk - Main.virtual_milk_consumption) > 0
-					&& (Main.sugar - Main.virtual_sugar_consumption) > 0) {
-				// remove the participant from the queue counter
-				Main.professor_queue_counter--;
-				// release him from the queue
-				Main.s_professor.release();
-
-				System.out.println("Student: " + id + " waking up a professor");
-
-				// keep track of the virtual consumption
-				Main.virtual_coffee_consumption++;
-				Main.virtual_milk_consumption++;
-				Main.virtual_sugar_consumption++;
-			}
-
-		}
-
-		for (int i = 0; i < Main.DOCTOR_COUNT; i++) {
-			if (Main.doctor_queue_counter > 0 && Main.coffee - Main.virtual_coffee_consumption > 0
-					&& Main.milk - Main.virtual_milk_consumption > 0) {
-				Main.doctor_queue_counter--;
-				Main.s_doctor.release();
-				System.out.println("Student: " + id + " waking up a doctor");
-
-				Main.virtual_coffee_consumption++;
-				Main.virtual_milk_consumption++;
-			}
-
-		}
-
-		for (int i = 0; i < Main.PHD_COUNT; i++) {
-			if (Main.phd_queue_counter > 0 && Main.coffee - Main.virtual_coffee_consumption> 0
-					&& Main.sugar - Main.virtual_sugar_consumption > 0) {
-				Main.phd_queue_counter--;
-				Main.s_phd.release();
-				System.out.println("Student: " + id + " waking up a phd");
-
-				Main.virtual_coffee_consumption++;
-				Main.virtual_sugar_consumption++;
-			}
-		}
-
-		for (int i = 0; i < Main.STUDENT_COUNT; i++) {
-			if (Main.student_queue_counter > 0 && Main.milk - Main.virtual_milk_consumption > 0
-					&& Main.sugar - Main.virtual_sugar_consumption > 0) {
-				Main.student_queue_counter--;
-				Main.s_student.release();
-				System.out.println("Student: " + id + " waking up a student");
-				
-				Main.virtual_milk_consumption++;
-				Main.virtual_sugar_consumption++;
-			}
-		}
 	}
 }
